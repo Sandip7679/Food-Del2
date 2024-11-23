f0ea526e529b4c799f8f84827aa5ccae34d9a944
Sandip Bera <107534834+Sandip7679@users.noreply.github.com>
2024-11-23 12:29:34 +0530

    railway api

--- src/Context/StoreContext.jsx
+++ src/Context/StoreContext.jsx
@@ -6,7 +6,8 @@ export const StoreContext = createContext(null);
 const StoreContextProvider = (props) => {
 
     // const url = "http://localhost:4000"
-    const url = "https://fooddel-backend-pf0z.onrender.com"
+    const url = "https://fooddelbackend.up.railway.app"
+    // const url = "https://fooddel-backend-pf0z.onrender.com"
     const [food_list, setFoodList] = useState([]);
     const [cartItems, setCartItems] = useState({});
     const [token, setToken] = useState("")

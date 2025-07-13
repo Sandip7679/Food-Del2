36fa011f0f253788097079b8698b4d3bd65d1d49
Sandip Bera <107534834+Sandip7679@users.noreply.github.com>
2025-07-13 11:04:54 +0530

    removed helmet

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,17 +3,17 @@ import Header from "../../components/Header/Header";
 import ExploreMenu from "../../components/ExploreMenu/ExploreMenu";
 import FoodDisplay from "../../components/FoodDisplay/FoodDisplay";
 import AppDownload from "../../components/AppDownload/AppDownload";
-import { Helmet } from "react-helmet-async";
+// import { Helmet } from "react-helmet-async";
 
 const Home = () => {
   const [category, setCategory] = useState("All");
 
   return (
     <>
-      <Helmet>
+      {/* <Helmet>
         <title>Home - FoodyMood</title>
         <meta name="description" content="Welcome to FoodyMood's home page!" />
-      </Helmet>
+      </Helmet> */}
       <Header />
       <ExploreMenu setCategory={setCategory} category={category} />
       <FoodDisplay category={category} setCategory={setCategory} />

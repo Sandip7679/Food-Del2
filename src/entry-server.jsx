f663326fab31bd230c24145050bf4bc39592d5f1
Sandip Bera <107534834+Sandip7679@users.noreply.github.com>
2025-07-13 18:30:47 +0530

    ssr for render

--- src/entry-server.jsx
+++ src/entry-server.jsx
@@ -21,8 +21,9 @@
 // }
 
 import React from 'react';
-import { App } from './App';
+// import App from './App.jsx';
 import { renderToString } from 'react-dom/server';
+import App from './App';
 
 export async function render() {
   const html = renderToString(<App />);
